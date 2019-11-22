346c5bee1c5cf8b17ded8eae434a575d86aaa4c0
J.S.Patrick <980141374@qq.com>
2019-11-22 15:46:24 +0800

    添加注释

--- src/functional/inspecter.js
+++ src/functional/inspecter.js
@@ -1,7 +1,11 @@
+/*
+* 检查器   通过实例化的echarts对象在调用setOption时
+* 会先通过该检查器的验证判断
+* 触发对应图表类型的数据检查
+* 最后将被处理之后的数据返回
+* */
 import tool from "../util/utils";
 import Trigger from './tirgger';
-
-
 
 class Inspect {
 	constructor(){
--- src/functional/tirgger.js
+++ src/functional/tirgger.js
@@ -1,3 +1,8 @@
+/*
+* 触发器   inspecter调用该触发器
+* 触发handler中的所有类型的参数判断
+* 同一种类型可以检查不同参数 验证并修改为预定的格式
+* */
 import { pieHandler } from "./handler/pie";
 
 class Tirgger {
